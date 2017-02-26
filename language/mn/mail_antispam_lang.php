0169a31f9cd4d5ee491f926f5bcc1478d272ee39
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:26:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam програм спам тодорхойлох Сэтгэн олох шинжилгээний өргөн хүрээтэй ашигладаг мэйл шүүлтүүр олгодог.';
+$lang['mail_antispam_app_name'] = 'Захидал Antispam';
+$lang['mail_antispam_blacklist'] = 'Хар жагсаалт';
+$lang['mail_antispam_list'] = 'Жагсаалт (нэг нэг мөрөнд и-мэйл)';
+$lang['mail_antispam_lists'] = 'Жагсаалтууд';
+$lang['mail_antispam_whitelist'] = 'Цагаан жагсаалтаар';
