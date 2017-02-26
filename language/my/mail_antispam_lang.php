697f1a3d12e6cac02f4dd0fb59eb3e35848fc548
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:20:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'အဆိုပါ Antispam app ကို spam များကိုဖော်ထုတ်ရန် heuristic စမ်းသပ်မှုတွေ၏ကျယ်ပြန့်အကွာအဝေးကိုအသုံးပြုတဲ့ mail ကိုစစ်ထုတ်သည်။';
+$lang['mail_antispam_app_name'] = 'မေးလ် Antispam';
+$lang['mail_antispam_blacklist'] = 'အမည်ပျက်စာရင်း';
+$lang['mail_antispam_list'] = 'စာရင်း (လိုင်းနှုန်းတယောက်အီးမေးလ်)';
+$lang['mail_antispam_lists'] = 'စာရင်း';
+$lang['mail_antispam_whitelist'] = 'whitelist';
