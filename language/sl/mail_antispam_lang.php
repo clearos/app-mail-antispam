93684c97ecccfb27af0c442a969b6b6ae844ce5b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:28:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Neželene pošte Aplikacija omogoča poštni filter, ki uporablja širok spekter hevrističnih testov za identifikacijo spam.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Črna lista';
+$lang['mail_antispam_list'] = 'Seznam (en email na vrstico)';
+$lang['mail_antispam_lists'] = 'seznami';
+$lang['mail_antispam_whitelist'] = 'Seznam dovoljenih';
