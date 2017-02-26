0919c5ef46b0934facc882fc95350b5bcd2a06c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:22:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Die Spam-Schutz App bietet eine E-Mail-Filter, der eine breite Palette von heuristischen Tests verwendet, um Spam zu identifizieren.';
+$lang['mail_antispam_app_name'] = 'Mail-Spam-Schutz';
+$lang['mail_antispam_blacklist'] = 'Schwarze Liste';
+$lang['mail_antispam_list'] = 'Liste (eine E-Mail pro Zeile)';
+$lang['mail_antispam_lists'] = 'Listen';
+$lang['mail_antispam_whitelist'] = 'Weiße Liste';
