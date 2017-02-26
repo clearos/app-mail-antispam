d48d85a9cd90204452160f1c8ebc9ba532da3410
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:19:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'The Antispam ծրագիրը ապահովում է փոստի ֆիլտր է, որ օգտագործում է լայն շրջանակ հերոստիկ թեստերի բացահայտել սպամ:';
+$lang['mail_antispam_app_name'] = 'Փոստ Antispam';
+$lang['mail_antispam_blacklist'] = 'սլ ցուցակ';
+$lang['mail_antispam_list'] = 'Ցուցակ (մեկ էլ յուրաքանչյուր տողում)';
+$lang['mail_antispam_lists'] = 'ցուցակները';
+$lang['mail_antispam_whitelist'] = 'Սպիտակ ցուցակ';
