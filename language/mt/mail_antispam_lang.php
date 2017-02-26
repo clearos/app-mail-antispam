6c830227b84b9ad8ff54db692473c3d5e06f8ccb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:26:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Il-app Antispam jipprovdi filtru mail li tuża firxa wiesgħa ta &#39;testijiet heuristic biex jiġu identifikati l-ispam.';
+$lang['mail_antispam_app_name'] = 'posta Antispam';
+$lang['mail_antispam_blacklist'] = 'Lista sewda';
+$lang['mail_antispam_list'] = 'Lista (wieħed posta elettronika għal kull linja)';
+$lang['mail_antispam_lists'] = 'listi ta';
+$lang['mail_antispam_whitelist'] = 'whitelist';
