8c1884c6c3591d4fc534fd0ce081880ef1c1a81f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:24:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'スパム対策アプリは、スパムを識別するためのヒューリスティックテストの広い範囲を使用してメールフィルタを提供します。';
+$lang['mail_antispam_app_name'] = 'メールアンチスパム';
+$lang['mail_antispam_blacklist'] = 'ブラックリスト';
+$lang['mail_antispam_list'] = 'リスト（1行に1つのメール）';
+$lang['mail_antispam_lists'] = 'リスト';
+$lang['mail_antispam_whitelist'] = 'ホワイトリスト';
