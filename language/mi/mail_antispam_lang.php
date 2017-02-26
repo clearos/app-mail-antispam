7d9aaa539076a21834c8ae98839f095a8376eb0a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:25:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'whakarato te taupānga Antispam te tātari mēra e whakamahi ana i te whānui o te whakamātautau heuristic whānui ki te tāutu i pāme.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Blacklist';
+$lang['mail_antispam_list'] = 'Rārangi (kotahi īmēra ia rārangi)';
+$lang['mail_antispam_lists'] = 'rārangi';
+$lang['mail_antispam_whitelist'] = 'whitelist';
