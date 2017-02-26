25e095add055985b020d9878262fe9226f57dc2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:22:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Rämpspost app annab mail filter, mis kasutab erinevaid heuristiline testid tuvastada rämpsposti.';
+$lang['mail_antispam_app_name'] = 'mail spämm';
+$lang['mail_antispam_blacklist'] = 'must nimekiri';
+$lang['mail_antispam_list'] = 'Nimekiri (ühe e rea kohta)';
+$lang['mail_antispam_lists'] = 'Nimekirjad';
+$lang['mail_antispam_whitelist'] = 'Whitelisti';
