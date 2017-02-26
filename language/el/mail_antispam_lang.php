4f8a226c6304de83db63b62ad965446234775e31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:22:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Η εφαρμογή Antispam παρέχει ένα φίλτρο αλληλογραφίας που χρησιμοποιεί ένα ευρύ φάσμα ευρετική δοκιμές για τον εντοπισμό spam.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Προγραφή';
+$lang['mail_antispam_list'] = 'Λίστα (ένα μήνυμα ηλεκτρονικού ταχυδρομείου ανά γραμμή)';
+$lang['mail_antispam_lists'] = 'Τόπος αγώνων';
+$lang['mail_antispam_whitelist'] = 'Λευκή Λίστα';
