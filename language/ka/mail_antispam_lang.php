833282c37930180f53861c0f4858b123f760c5b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:24:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam ოთახი უზრუნველყოფს mail ფილტრი, რომელიც იყენებს ფართო სპექტრი ევრისტიკული ტესტები იდენტიფიცირება spam.';
+$lang['mail_antispam_app_name'] = 'ფოსტა Antispam';
+$lang['mail_antispam_blacklist'] = 'blacklist';
+$lang['mail_antispam_list'] = 'სია (ერთი ელ თითო ხაზი)';
+$lang['mail_antispam_lists'] = 'სიები';
+$lang['mail_antispam_whitelist'] = 'whitelist';
