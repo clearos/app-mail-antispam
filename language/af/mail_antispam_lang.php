314a54ef29a7c53a5a0961fde18457e636861c7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:19:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Die Antispam app bied &#39;n e-pos filter wat &#39;n wye verskeidenheid van heuristiese toetse gebruik om spam te identifiseer.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'swartlys';
+$lang['mail_antispam_list'] = 'Lys (een e-pos per lyn)';
+$lang['mail_antispam_lists'] = 'lyste';
+$lang['mail_antispam_whitelist'] = 'witlys';
