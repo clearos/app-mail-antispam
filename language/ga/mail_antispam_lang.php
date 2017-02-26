20860f8e70e35c8aebd5928e7645e641802a4636
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:23:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Soláthraíonn an app Antispam scagaire phost go n-úsáideann raon leathan tástálacha heorastúla spam a aithint.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'blacklist';
+$lang['mail_antispam_list'] = 'List (amháin r-phost in aghaidh an líne)';
+$lang['mail_antispam_lists'] = 'liostaí';
+$lang['mail_antispam_whitelist'] = 'whitelist';
