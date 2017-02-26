dbb8c166e2ccc8dfd1ff3c20b93c1bfd6b75c602
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:25:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'ການ app Antispam ສະຫນອງການກັ່ນຕອງ mail ທີ່ໃຊ້ເປັນລະດັບຄວາມກ້ວາງຂອງການທົດສອບ heuristic ເພື່ອກໍານົດຂີ້ເຫຍື້ອ.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Blacklist';
+$lang['mail_antispam_list'] = 'ບັນຊີລາຍຊື່ (ຫນຶ່ງຕໍ່ອອນໄລນ໌)';
+$lang['mail_antispam_lists'] = 'ລາຍການ';
+$lang['mail_antispam_whitelist'] = 'ບັນຊີຂາວ';
