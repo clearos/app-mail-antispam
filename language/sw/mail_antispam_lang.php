d3927c4abd0211721f88ff9a373cdba4490d8a0f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:29:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'programu Antispam hutoa mail filter ambayo inatumia mbalimbali ya vipimo heuristic kutambua barua taka.';
+$lang['mail_antispam_app_name'] = 'Barua Antispam';
+$lang['mail_antispam_blacklist'] = 'blacklist';
+$lang['mail_antispam_list'] = 'Orodha (moja email kwa kila mstari)';
+$lang['mail_antispam_lists'] = 'orodha';
+$lang['mail_antispam_whitelist'] = 'whitelist';
