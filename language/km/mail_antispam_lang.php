ff03bcc80830ffc4c439d037da222c7db153f33c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:24:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'កម្មវិធីឃាឃាំងផ្ដល់នូវតម្រងសំបុត្រដែលប្រើទូលំទូលាយមួយនៃការធ្វើតេស្តដើម្បីកំណត់អត្តសញ្ញាណសារឥតបានការ heuristic ។';
+$lang['mail_antispam_app_name'] = 'សំបុត្រឃាឃាំង';
+$lang['mail_antispam_blacklist'] = 'បញ្ជីខ្មៅ';
+$lang['mail_antispam_list'] = 'បញ្ជី (មួយអ៊ីម៉ែលក្នុងមួយបន្ទាត់)';
+$lang['mail_antispam_lists'] = 'បញ្ជី';
+$lang['mail_antispam_whitelist'] = 'បញ្ជីស';
