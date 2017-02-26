54f81aec96213006bf2e0dcf25f0022c73e94532
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:25:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam programa suteikia pašto filtrą, kuris naudoja platų euristiniais bandymų nustatyti šlamštas.';
+$lang['mail_antispam_app_name'] = 'paštas antispam';
+$lang['mail_antispam_blacklist'] = 'juodasis sąrašas';
+$lang['mail_antispam_list'] = 'Sąrašas (vienas laiškas per line)';
+$lang['mail_antispam_lists'] = 'sąrašai';
+$lang['mail_antispam_whitelist'] = 'whitelist';
