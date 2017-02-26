2d7f5b0ea38f5525d0e9720eac700d22518eadb2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:29:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam பயன்பாட்டை ஸ்பேம் அடையாளம் பட்டறிவு சோதனைகள் ஒரு பரவலான பயன்படுத்துகிறது என்று ஒரு மெயில் வடிகட்டி வழங்குகிறது.';
+$lang['mail_antispam_app_name'] = 'மின்னஞ்சல் Antispam';
+$lang['mail_antispam_blacklist'] = 'பிளாக்லிஸ்ட்';
+$lang['mail_antispam_list'] = 'பட்டியல் (ஒரு சதவீதம் வரி மின்னஞ்சல்)';
+$lang['mail_antispam_lists'] = 'பட்டியல்கள்';
+$lang['mail_antispam_whitelist'] = 'அனுமதிப்பட்டியல்';
