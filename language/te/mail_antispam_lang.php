9f36316830a892944f744d74b7969004ca5c20f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:29:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam అనువర్తనం స్పామ్ గుర్తించడానికి పరిష్కార పరీక్షలు విస్తృత ఉపయోగించే ఒక మెయిల్ వడపోత అందిస్తుంది.';
+$lang['mail_antispam_app_name'] = 'మెయిల్ Antispam';
+$lang['mail_antispam_blacklist'] = 'బ్లాక్లిస్ట్';
+$lang['mail_antispam_list'] = 'జాబితా (ఒక పంక్తికి ఇమెయిల్)';
+$lang['mail_antispam_lists'] = 'జాబితాలు';
+$lang['mail_antispam_whitelist'] = 'వైట్ లిస్ట్';
