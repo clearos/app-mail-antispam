965a1cd7e59d4bd2800823850388151123b7526c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:26:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Aplikasi Antispam menyediakan penapis mel yang menggunakan pelbagai ujian heuristik untuk mengenal pasti spam.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Senarai hitam';
+$lang['mail_antispam_list'] = 'Senarai (satu e-mel bagi setiap baris)';
+$lang['mail_antispam_lists'] = 'senarai';
+$lang['mail_antispam_whitelist'] = 'Senarai Putih';
