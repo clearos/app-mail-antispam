1d25696dea41377ee65dbedff9cb9a223438b208
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:30:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam uygulaması spam belirlemek için sezgisel testlerin geniş bir yelpazede kullanan bir e-posta filtresi sağlar.';
+$lang['mail_antispam_app_name'] = 'posta Antispam';
+$lang['mail_antispam_blacklist'] = 'kara liste';
+$lang['mail_antispam_list'] = 'Liste (satır başına bir e-posta)';
+$lang['mail_antispam_lists'] = 'Listeler';
+$lang['mail_antispam_whitelist'] = 'Beyaz liste';
