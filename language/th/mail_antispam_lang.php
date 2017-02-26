e53e4164951c66da7592b957322845f43df5820e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:29:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'แอปพลิเค Antispam ให้ตัวกรองอีเมลที่ใช้ช่วงกว้างของการทดสอบการแก้ปัญหาเพื่อแจ้งสแปม';
+$lang['mail_antispam_app_name'] = 'จดหมาย Antispam';
+$lang['mail_antispam_blacklist'] = 'บัญชีดำ';
+$lang['mail_antispam_list'] = 'รายการ (หนึ่งอีเมล์ต่อบรรทัด)';
+$lang['mail_antispam_lists'] = 'รายการ';
+$lang['mail_antispam_whitelist'] = 'Whitelist';
