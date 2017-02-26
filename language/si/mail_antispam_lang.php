8a678f9fa091d333484d049aeb48c8ec7d9fff47
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:28:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'මෙම Antispam ස්පෑම් හඳුනා ගැනීමට කලපුවේ කිලෝමීටරයක් ​​පරීක්ෂණ පුළුල් පරාසයක භාවිතා කරන තැපැල පෙරහන සපයයි.';
+$lang['mail_antispam_app_name'] = 'තැපැල් Antispam';
+$lang['mail_antispam_blacklist'] = 'ට තුවාලයි';
+$lang['mail_antispam_list'] = 'ලැයිස්තුව (එක් පේළියකට එක් ඊ-තැපෑල)';
+$lang['mail_antispam_lists'] = 'ලැයිස්තු';
+$lang['mail_antispam_whitelist'] = 'සුදු ලැයිස්තුවේ';
