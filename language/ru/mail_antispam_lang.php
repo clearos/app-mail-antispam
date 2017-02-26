53ab99b04d16d9c9874a6ad6cec716313aeb18eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:27:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Приложение Antispam предоставляет почтовый фильтр, который использует широкий спектр эвристических тестов для выявления спама.';
+$lang['mail_antispam_app_name'] = 'Почта Antispam';
+$lang['mail_antispam_blacklist'] = 'Черный список';
+$lang['mail_antispam_list'] = 'Список (один адрес электронной почты в строке)';
+$lang['mail_antispam_lists'] = 'Списки';
+$lang['mail_antispam_whitelist'] = 'Whitelist';
