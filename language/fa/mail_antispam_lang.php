fa0f29d2106af0687cbadbd3eaac9f0cadbd82ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:27:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'برنامه کاسپراسکای فراهم می کند یک فیلتر ایمیل استفاده می کند که طیف گسترده ای از تست های اکتشافی برای شناسایی اسپم.';
+$lang['mail_antispam_app_name'] = 'ایمیل کاسپراسکای';
+$lang['mail_antispam_blacklist'] = 'لیست سیاه';
+$lang['mail_antispam_list'] = 'فهرست (یک ایمیل در هر خط)';
+$lang['mail_antispam_lists'] = 'لیست';
+$lang['mail_antispam_whitelist'] = 'لیست سفید';
