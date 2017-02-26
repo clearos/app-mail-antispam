21d083cd12c04a5e76f0de06714990f1c9b7df02
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:26:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'यो Antispam अनुप्रयोग मेल फिल्टर स्प्याम पहिचान गर्न अनुमानी परीक्षण को एक विस्तृत श्रृंखला प्रयोग प्रदान गर्दछ।';
+$lang['mail_antispam_app_name'] = 'मेल Antispam';
+$lang['mail_antispam_blacklist'] = 'कालोसूची';
+$lang['mail_antispam_list'] = 'सूची (रेखामा एउटा इमेल)';
+$lang['mail_antispam_lists'] = 'सूची';
+$lang['mail_antispam_whitelist'] = 'स्वेतसूची';
