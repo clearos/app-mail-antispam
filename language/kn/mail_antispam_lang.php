6c31439cce9175e307e601cfd5b6333a5d11ff44
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:24:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'ಆಂಟಿಸ್ಪ್ಯಾಮ್ ಅಪ್ಲಿಕೇಶನ್ ಸ್ಪ್ಯಾಮ್ ಗುರುತಿಸಲು ಅನ್ವೇಷಣ ಪರೀಕ್ಷೆಗಳು ವ್ಯಾಪಕ ಬಳಸುವ ಒಂದು ಮೇಲ್ ಫಿಲ್ಟರ್ ಒದಗಿಸುತ್ತದೆ.';
+$lang['mail_antispam_app_name'] = 'ಮೇಲ್ ಆಂಟಿಸ್ಪ್ಯಾಮ್';
+$lang['mail_antispam_blacklist'] = 'ಕಪ್ಪುಪಟ್ಟಿ';
+$lang['mail_antispam_list'] = 'ಪಟ್ಟಿ (ಒಂದು ಸಾಲಿಗೆ ಇಮೇಲ್)';
+$lang['mail_antispam_lists'] = 'ಪಟ್ಟಿಗಳು';
+$lang['mail_antispam_whitelist'] = 'ಬಿಳಿಪಟ್ಟಿ';
