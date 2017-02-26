c33ea8870180bc7be3364a30a110d0787749097c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:30:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Mae&#39;r app Antispam yn darparu hidlo post sy&#39;n defnyddio amrediad eang o brofion hewristig i adnabod sbam.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Rhestr Ddu';
+$lang['mail_antispam_list'] = 'Rhestr (un e-bost i bob llinell)';
+$lang['mail_antispam_lists'] = 'rhestrau';
+$lang['mail_antispam_whitelist'] = 'rhestr wen';
