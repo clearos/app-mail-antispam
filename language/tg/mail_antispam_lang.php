e04926f140d954886c04916d4871ddbdc8851d82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:29:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Дар барнома Antispam таъмин филтр почтаи электронӣ, ки истифода мебарад, доираи васеи санҷишҳои heuristic муайян спам.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'феҳрасте';
+$lang['mail_antispam_list'] = 'Рӯйхати (як почтаи электронӣ дар ҳар сатр)';
+$lang['mail_antispam_lists'] = 'рӯйхати';
+$lang['mail_antispam_whitelist'] = 'Whitelist';
