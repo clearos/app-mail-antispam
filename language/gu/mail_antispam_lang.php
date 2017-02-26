b67475189289c15ab0fc70abe52a86ddee017fc0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:22:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam એપ્લિકેશન સંશોધનાત્મક પરીક્ષણો વિશાળ શ્રેણી વાપરે છે કે જે સ્પામ ઓળખવા માટે એક મેલ ફિલ્ટર પૂરી પાડે છે.';
+$lang['mail_antispam_app_name'] = 'મેઇલ Antispam';
+$lang['mail_antispam_blacklist'] = 'બ્લેકલિસ્ટ';
+$lang['mail_antispam_list'] = 'યાદી (લાઇન દીઠ એક ઇમેઇલ)';
+$lang['mail_antispam_lists'] = 'યાદી આપે છે';
+$lang['mail_antispam_whitelist'] = 'વ્હાઇટલિસ્ટ';
