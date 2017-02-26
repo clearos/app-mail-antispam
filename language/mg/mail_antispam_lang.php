128b6e8f636230d79943ce737a52a22c412c38b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:25:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Ny fampiharana Antispam manome namany Sary sivana izay mampiasa isan-karazany ny heuristic fitsapana mba hamantatra spam.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Blacklist';
+$lang['mail_antispam_list'] = 'List (mailaka iray isaky ny andalana)';
+$lang['mail_antispam_lists'] = 'Lists';
+$lang['mail_antispam_whitelist'] = 'Whitelist';
