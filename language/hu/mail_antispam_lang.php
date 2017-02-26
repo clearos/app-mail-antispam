dd83185d9cee1c71ee33797a4148ee4639d3bf7c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:23:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'A Antispam app nyújt mail szűrő, amely széles körű heurisztikus tesztek azonosítására spam.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Feketelista';
+$lang['mail_antispam_list'] = 'List (egy e-mailt soronként)';
+$lang['mail_antispam_lists'] = 'listák';
+$lang['mail_antispam_whitelist'] = 'Whitelist';
