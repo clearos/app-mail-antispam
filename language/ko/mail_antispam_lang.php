be6a07b1f4ef255d2d18e111da0faf738a4b9183
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:24:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = '스팸 방지 앱은 스팸을 식별하는 휴리스틱 검사의 광범위한 사용하는 메일 필터를 제공합니다.';
+$lang['mail_antispam_app_name'] = '메일 스팸 방지';
+$lang['mail_antispam_blacklist'] = '블랙리스트';
+$lang['mail_antispam_list'] = '목록 (한 줄에 하나씩 이메일)';
+$lang['mail_antispam_lists'] = '기울기';
+$lang['mail_antispam_whitelist'] = '화이트리스트';
