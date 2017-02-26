a62b70a7291e433f4f611503c1ad1a0e29301eb4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:21:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'The app Antispam imapereka makalata sefa kuti amagwiritsa ntchito zosiyanasiyana mayesero heuristic kuzindikira sipamu.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Blacklist';
+$lang['mail_antispam_list'] = 'List (wina imelo pa mzere)';
+$lang['mail_antispam_lists'] = 'Wapamwamba';
+$lang['mail_antispam_whitelist'] = 'Zovomerezeka';
