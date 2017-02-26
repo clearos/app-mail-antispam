05fd69449841d3698194cc70b84d804cf4e8142b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:23:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'The Antispam app veitir póst síu sem notar margs konar leitandi prófum til að bera kennsl á ruslpóst.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Svartur listi';
+$lang['mail_antispam_list'] = 'List (einn tölvupóstur á línu)';
+$lang['mail_antispam_lists'] = 'listar';
+$lang['mail_antispam_whitelist'] = 'hvítlista';
