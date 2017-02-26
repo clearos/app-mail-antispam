9051c461ed01997c75dd037ab39dd9c844b607d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:20:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam অ্যাপ্লিকেশন অনুসন্ধানমূলক পরীক্ষার একটি বিস্তৃত ব্যবহার করে স্প্যাম সনাক্ত করার জন্য একটি মেইল ​​ফিল্টার প্রদান করে.';
+$lang['mail_antispam_app_name'] = 'মেল Antispam';
+$lang['mail_antispam_blacklist'] = 'নিষিদ্ধ জিনিসের তালিকা';
+$lang['mail_antispam_list'] = 'তালিকা (প্রতি লাইনে একটি ইমেইল)';
+$lang['mail_antispam_lists'] = 'পাখি';
+$lang['mail_antispam_whitelist'] = 'হোয়াইটলিস্ট';
