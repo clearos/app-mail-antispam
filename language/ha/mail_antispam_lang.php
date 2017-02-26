2843a03b9ca595a25bbb726398fd28693bce565e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:23:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'The Antispam app samar da wani mail tace cewa yana amfani da mai fadi da kewayon heuristic gwaje-gwaje don gane spam.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Blacklist';
+$lang['mail_antispam_list'] = 'List (daya email da line)';
+$lang['mail_antispam_lists'] = 'lists';
+$lang['mail_antispam_whitelist'] = 'jerin ma&#39;aikata';
