6b66634f2c05a852ce477fd01008bb5cfc1455ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:30:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Các ứng dụng Antispam cung cấp một bộ lọc mail sử dụng một loạt các kiểm tra heuristic để nhận dạng thư rác.';
+$lang['mail_antispam_app_name'] = 'thư Antispam';
+$lang['mail_antispam_blacklist'] = 'Danh sách đen';
+$lang['mail_antispam_list'] = 'Danh sách (một email mỗi dòng)';
+$lang['mail_antispam_lists'] = 'Chức năng';
+$lang['mail_antispam_whitelist'] = 'Whitelist';
