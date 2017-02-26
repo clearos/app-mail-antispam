8eda9b30103a1acb7205f7fe56d853f84095f18f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:25:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam app sniedz pasta filtru, kas izmanto plašu heiristisko testu, lai noteiktu surogātpastu.';
+$lang['mail_antispam_app_name'] = 'pasts Antispam';
+$lang['mail_antispam_blacklist'] = 'ierakstīt melnajā sarakstā';
+$lang['mail_antispam_list'] = 'Saraksts (viens e-pasts katrā rindā)';
+$lang['mail_antispam_lists'] = 'saraksti';
+$lang['mail_antispam_whitelist'] = 'baltais saraksts';
