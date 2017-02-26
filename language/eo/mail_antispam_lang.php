ad32ac98cc9af79501549d63a26fe7f72e45dba0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:21:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'La Antispam app provizas poŝto filtrilo kiu uzas larĝan gamon de heŭristiko provoj identigi spamon.';
+$lang['mail_antispam_app_name'] = 'poŝto Antispam';
+$lang['mail_antispam_blacklist'] = 'blacklist';
+$lang['mail_antispam_list'] = 'Listo (unu retpoŝto por linio)';
+$lang['mail_antispam_lists'] = 'listoj';
+$lang['mail_antispam_whitelist'] = 'whitelist';
