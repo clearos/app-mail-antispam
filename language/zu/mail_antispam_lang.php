0756afea71bc8501b410f9fee7bc9d3a5661d9a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:31:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Uhlelo lokusebenza Antispam ihlinzeka lemifanekiso mail olusebenzisa anhlobonhlobo izivivinyo heuristic ukukhomba spam.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Blacklist';
+$lang['mail_antispam_list'] = 'Uhlu (owodwa imeyili line ngamunye)';
+$lang['mail_antispam_lists'] = 'Lists';
+$lang['mail_antispam_whitelist'] = 'yokugunyaza';
