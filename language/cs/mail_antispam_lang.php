e573b664ceb989a2c140988d9babc7654fe241ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:21:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Spamového aplikace poskytuje e-mailový filtr, který používá širokou škálu heuristických testů k identifikaci spamu.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Černá listina';
+$lang['mail_antispam_list'] = 'List (jeden e-mail na řádek)';
+$lang['mail_antispam_lists'] = 'seznamy';
+$lang['mail_antispam_whitelist'] = 'whitelist';
