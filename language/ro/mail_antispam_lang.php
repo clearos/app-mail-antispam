20252dd088345bd4581f7e405532a32e6f2c8268
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:27:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Aplicația Antispam oferă un filtru de e-mail care utilizează o gamă largă de teste pentru a identifica spam-ul euristice.';
+$lang['mail_antispam_app_name'] = 'Antispam e-mail';
+$lang['mail_antispam_blacklist'] = 'blacklist';
+$lang['mail_antispam_list'] = 'Lista (un e-mail pe fiecare rând)';
+$lang['mail_antispam_lists'] = 'liste';
+$lang['mail_antispam_whitelist'] = 'Listă albă';
