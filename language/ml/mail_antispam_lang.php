e0ea6ff0b033ae7fd75014e6e16644104be7ffa7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:26:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam അപ്ലിക്കേഷൻ സ്പാം തിരിച്ചറിയാൻ അനുമാന പരിശോധനകൾക്കും വൈവിധ്യമാർന്ന ഉപയോഗിക്കുന്ന ഒരു മെയിൽ ഫിൽട്ടർ നൽകുന്നു.';
+$lang['mail_antispam_app_name'] = 'മെയിൽ Antispam';
+$lang['mail_antispam_blacklist'] = 'കരിമ്പട്ടിക';
+$lang['mail_antispam_list'] = 'പട്ടിക (ലൈനിന് ഒരു ഇമെയിൽ)';
+$lang['mail_antispam_lists'] = 'ലിസ്റ്റുകൾ';
+$lang['mail_antispam_whitelist'] = 'വൈറ്റ്ലിസ്റ്റ്';
