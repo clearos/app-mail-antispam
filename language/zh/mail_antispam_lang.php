c61fbe67c3f693f65ba443a36d0c7238d07b81d3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:21:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = '反垃圾邮件应用程序提供了一个使用广泛的启发式测试，以识别垃圾邮件的邮件过滤器。';
+$lang['mail_antispam_app_name'] = '反垃圾邮件';
+$lang['mail_antispam_blacklist'] = '黑名单';
+$lang['mail_antispam_list'] = '列表（每行一个电子邮件）';
+$lang['mail_antispam_lists'] = '清单';
+$lang['mail_antispam_whitelist'] = '白名单';
