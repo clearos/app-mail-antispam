42c052a438bd2544dc8d2cb20a4c66f0a0dceaa3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:27:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam ਐਪ ਮਹੱਤਵਪੂਰਨ ਟੈਸਟ ਦੀ ਇੱਕ ਵਿਆਪਕ ਲੜੀ ਨੂੰ ਵਰਤਦਾ ਹੈ, ਜੋ ਕਿ ਸਪੈਮ ਦੀ ਪਛਾਣ ਕਰਨ ਲਈ ਇੱਕ ਪੱਤਰ ਫਿਲਟਰ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ.';
+$lang['mail_antispam_app_name'] = 'ਮੇਲ Antispam';
+$lang['mail_antispam_blacklist'] = 'ਬਲੈਕਲਿਸਟ';
+$lang['mail_antispam_list'] = 'ਸੂਚੀ (ਇੱਕ ਪ੍ਰਤੀ ਲਾਈਨ ਈ-ਮੇਲ)';
+$lang['mail_antispam_lists'] = 'ਸੂਚੀ';
+$lang['mail_antispam_whitelist'] = 'ਵਾਈਟਲਿਸਟ';
