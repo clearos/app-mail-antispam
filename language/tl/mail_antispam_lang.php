9581c72a4e15197dbf0918d7f6afe09405c5228e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:29:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Ang Antispam app ay nagbibigay ng isang mail filter na gumagamit ng isang malawak na hanay ng hyuristiko pagsusuri upang makilala ang spam.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'talaang-itim';
+$lang['mail_antispam_list'] = 'List (isang email sa bawat linya)';
+$lang['mail_antispam_lists'] = 'listahan';
+$lang['mail_antispam_whitelist'] = 'whitelist';
