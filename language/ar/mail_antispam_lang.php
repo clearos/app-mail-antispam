f3fac0a70dd2bd5ff3d46760cda4be07bb012487
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:19:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'ويوفر التطبيق التطفلي تصفية البريد الذي يستخدم مجموعة واسعة من اختبارات الكشف عن مجريات الأمور لتحديد البريد المزعج.';
+$lang['mail_antispam_app_name'] = 'البريد التطفلي';
+$lang['mail_antispam_blacklist'] = 'القائمة السوداء';
+$lang['mail_antispam_list'] = 'قائمة (بريد إلكتروني واحد في كل سطر)';
+$lang['mail_antispam_lists'] = 'قوائم';
+$lang['mail_antispam_whitelist'] = 'القائمة البيضاء';
