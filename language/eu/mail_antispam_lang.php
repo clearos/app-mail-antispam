d33c08a0972032cd5cd2a69a18bccce87d6b3def
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:20:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam Aplikazioa heuristiko probak sorta zabal bat erabiltzen spam identifikatzeko mail iragazki bat eskaintzen du.';
+$lang['mail_antispam_app_name'] = 'Mail antispam';
+$lang['mail_antispam_blacklist'] = 'Zerrenda beltza';
+$lang['mail_antispam_list'] = 'Zerrenda (lerro bakoitzeko email bat)';
+$lang['mail_antispam_lists'] = 'zerrendak';
+$lang['mail_antispam_whitelist'] = 'Zerrenda zuria';
