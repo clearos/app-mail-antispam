53c8022ef8d3493c06871da17c64466741e1a826
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:30:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Antispam ilova spam aniqlash uchun evristik testlar keng ko&#39;lamli foydalanadi a mail filtri beradi.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'Filtr-Qora ro&#39;yxat';
+$lang['mail_antispam_list'] = 'Ro&#39;yxati (bir satrda bir email)';
+$lang['mail_antispam_lists'] = 'Do&#39;slashish';
+$lang['mail_antispam_whitelist'] = 'oq ro&#39;yxati';
