542e789d3ee6ac9e1781aab8c748cac516090074
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:30:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'اینٹی سپیم اے پی پی کے ایک میل فلٹر سپیم سے شناخت کے لئے انکشافی ٹیسٹ کی ایک وسیع رینج کو استعمال کرتا ہے فراہم کرتا ہے.';
+$lang['mail_antispam_app_name'] = 'میل اینٹی سپیم';
+$lang['mail_antispam_blacklist'] = 'بلیک لسٹ';
+$lang['mail_antispam_list'] = 'فہرست (ایک فی لائن ای میل)';
+$lang['mail_antispam_lists'] = 'فہرستیں';
+$lang['mail_antispam_whitelist'] = 'وائٹ لسٹ';
