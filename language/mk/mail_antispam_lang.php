70e5b2cd9e2cd4d0088367ab839e1734d3d1a8cb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:25:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Во стан Антиспам обезбедува пошта филтер, кој користи широк спектар на хеуристична тестови за да се идентификуваат спам.';
+$lang['mail_antispam_app_name'] = 'пошта Антиспам';
+$lang['mail_antispam_blacklist'] = 'црната листа';
+$lang['mail_antispam_list'] = 'Листа (еден е-мејл по линија)';
+$lang['mail_antispam_lists'] = 'листи';
+$lang['mail_antispam_whitelist'] = 'Белата листа';
