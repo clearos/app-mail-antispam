494f17694880c3a024538827a5c4eb946d1bd218
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:22:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'Roskapostin sovellus tarjoaa postia suodatin, joka käyttää monenlaisia ​​heuristisia testien tunnistaa roskapostin.';
+$lang['mail_antispam_app_name'] = 'mail Antispam';
+$lang['mail_antispam_blacklist'] = 'musta lista';
+$lang['mail_antispam_list'] = 'List (yksi sähköpostiosoite per rivi)';
+$lang['mail_antispam_lists'] = 'Listat';
+$lang['mail_antispam_whitelist'] = 'Valkoinen lista';
