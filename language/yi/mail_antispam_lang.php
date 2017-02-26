7b502756ef1c92ede0afda3e56c71e64a900a946
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:31:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'די אַנטיספּאַם אַפּ גיט אַ פּאָסט פילטער אַז ניצט אַ ברייט קייט פון כיוריסטיק טעסץ צו ידענטיפיצירן ספּאַם.';
+$lang['mail_antispam_app_name'] = 'מעיל אַנטיספּאַם';
+$lang['mail_antispam_blacklist'] = 'בלאַקקליסט';
+$lang['mail_antispam_list'] = 'רשימה (איין בליצפּאָסט פּער שורה)';
+$lang['mail_antispam_lists'] = 'ליס';
+$lang['mail_antispam_whitelist'] = 'ווהיטעליסט';
