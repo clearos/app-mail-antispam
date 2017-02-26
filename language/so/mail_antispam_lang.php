7a5ee8cf6e9c58b5554095f083a29a199e079ca6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:28:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/mail_antispam_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['mail_antispam_app_description'] = 'app Antispam wuxuu bixiyaa filter mail in uu isticmaalaa tiro balaadhan oo ah imtixaanka heuristic si ay u aqoonsadaan spam ah.';
+$lang['mail_antispam_app_name'] = 'Mail Antispam';
+$lang['mail_antispam_blacklist'] = 'liiska madoow';
+$lang['mail_antispam_list'] = 'List (hal email per line)';
+$lang['mail_antispam_lists'] = 'Liisaska';
+$lang['mail_antispam_whitelist'] = 'Liiska Mudnaanta';
